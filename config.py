83a2c1f39ebe45eeedb491eca60fdcd68a7adf7f
Ernesto Humpierres <ernesto.humpierres@gmail.com>
2024-09-22 20:08:26 +0000

    tt

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(32)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    #if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-    #    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Email configuration
