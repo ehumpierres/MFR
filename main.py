30426786e696da4eb674afa2119def523c3d65fd
Ernesto Humpierres <ernesto.humpierres@gmail.com>
2024-09-23 22:09:12 +0000

    testing init db

--- main.py
+++ main.py
@@ -139,6 +139,7 @@ def index():
 def login():
     form = LoginForm()
     try:
+        init_db()
         if form.validate_on_submit():
             admin_user = User.query.filter_by(username='admin').first()
             regular_user = User.query.filter_by(username='user').first()
@@ -539,7 +540,6 @@ def init_db():
         create_sample_data()
 
 if __name__ == '__main__':
-    init_db()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
